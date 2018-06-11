66752934d526b6257312a7309979a44fb78a3da8
Paul Hagedorn <panzer1119@gmail.com>
2018-06-11 19:04:52 +0200

    Added information
    Author etc.

--- aes_keygen.lua
+++ aes_keygen.lua
@@ -1,1 +1,11 @@
+--[[
 
+  Author: Panzer1119
+  
+  Date: Created 11 Jun 2018 - 07:03 PM
+  
+  Original Source: https://github.com/Panzer1119/CCAES/blob/master/aes_keygen.lua
+  
+  Direct Download: https://raw.githubusercontent.com/Panzer1119/CCAES/master/aes_keygen.lua
+
+]]--
